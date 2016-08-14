79ea52534ede6c12d3eedb2022114ae2a18380c9
baiqiangli <baiqiangli@didichuxing.com>
2016-08-14 22:36:32 +0800

    发号器注释

--- weed/sequence/memory_sequencer.go
+++ weed/sequence/memory_sequencer.go
@@ -5,24 +5,33 @@ import (
 )
 
 // just for testing
+//内存计数器结构
 type MemorySequencer struct {
-	counter      uint64
+	//计数器
+	counter uint64
+	//锁
 	sequenceLock sync.Mutex
 }
 
+//内存计数器构造函数
 func NewMemorySequencer() (m *MemorySequencer) {
 	m = &MemorySequencer{counter: 1}
 	return
 }
 
+//下一个文件id
 func (m *MemorySequencer) NextFileId(count uint64) (uint64, uint64) {
+	//加锁
 	m.sequenceLock.Lock()
 	defer m.sequenceLock.Unlock()
+	//取数
 	ret := m.counter
+	//对计数器+count
 	m.counter += uint64(count)
 	return ret, count
 }
 
+//设置新值
 func (m *MemorySequencer) SetMax(seenValue uint64) {
 	m.sequenceLock.Lock()
 	defer m.sequenceLock.Unlock()
@@ -31,6 +40,7 @@ func (m *MemorySequencer) SetMax(seenValue uint64) {
 	}
 }
 
+//查看当前值
 func (m *MemorySequencer) Peek() uint64 {
 	return m.counter
 }
--- weed/sequence/sequence.go
+++ weed/sequence/sequence.go
@@ -1,5 +1,6 @@
 package sequence
 
+//发号器接口
 type Sequencer interface {
 	NextFileId(count uint64) (uint64, uint64)
 	SetMax(uint64)
