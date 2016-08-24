4341f0ea328f7263cce392554ee86773088d27f4
baiqiangli <baiqiangli@didichuxing.com>
2016-08-24 08:38:13 +0800

    针文件的内存索引hash

--- weed/storage/needle_map_memory.go
+++ weed/storage/needle_map_memory.go
@@ -7,16 +7,20 @@ import (
 	"github.com/chrislusf/seaweedfs/weed/glog"
 )
 
+//真文件的map
 type NeedleMap struct {
 	m CompactMap
-
+	//基类
 	baseNeedleMapper
 }
 
+//NeedleMap的构造函数
 func NewNeedleMap(file *os.File) *NeedleMap {
+	//初始化m
 	nm := &NeedleMap{
 		m: NewCompactMap(),
 	}
+	//对indexFile进行赋值
 	nm.indexFile = file
 	return nm
 }
@@ -25,7 +29,9 @@ const (
 	RowsToRead = 1024
 )
 
+//加载NeedleMap
 func LoadNeedleMap(file *os.File) (*NeedleMap, error) {
+	//先初始化NeedleMap
 	nm := NewNeedleMap(file)
 	e := WalkIndexFile(file, func(key uint64, offset, size uint32) error {
 		if key > nm.MaximumFileKey {
@@ -83,24 +89,39 @@ func WalkIndexFile(r *os.File, fn func(key uint64, offset, size uint32) error) e
 	return e
 }
 
+//设置,新增或者修改
 func (nm *NeedleMap) Put(key uint64, offset uint32, size uint32) error {
 	oldSize := nm.m.Set(Key(key), offset, size)
+	//记录日志，记录最大的key，删除的次数，和大小，增加的次数和大小
 	nm.logPut(key, oldSize, size)
+	//追加到索引文件
 	return nm.appendToIndexFile(key, offset, size)
 }
+
+//查找,指定key对应的内容,先找slice，再找 map，再找slice
 func (nm *NeedleMap) Get(key uint64) (element *NeedleValue, ok bool) {
 	element, ok = nm.m.Get(Key(key))
 	return
 }
+
+//删除指定的key,先找cm中的index，然后在把slice中的size设置为0，然后如果在溢出表里面，删除,为啥还有可能在溢出表呢
 func (nm *NeedleMap) Delete(key uint64) error {
+	//删除内容
 	deletedBytes := nm.m.Delete(Key(key))
+	//记录删除的次数和删除的内容的大小
 	nm.logDelete(deletedBytes)
+	//为啥还更新到索引文件中呢,是为了重启时从从磁盘重建？
 	return nm.appendToIndexFile(key, 0, 0)
 }
+
+//关闭打开的索引文件
 func (nm *NeedleMap) Close() {
+	//close nm的indexFile文件句柄
 	_ = nm.indexFile.Close()
 }
+
+//销毁
 func (nm *NeedleMap) Destroy() error {
-	nm.Close()
-	return os.Remove(nm.indexFile.Name())
+	nm.Close()                            //先关闭nm
+	return os.Remove(nm.indexFile.Name()) //删除索引文件
 }
