eba086cfc0dde0cf324d01b58875a45c9edadb93
baiqiangli <baiqiangli@didichuxing.com>
2016-08-29 22:31:34 +0800

    索引文件

--- weed/tools/read_index.go
+++ weed/tools/read_index.go
@@ -1,5 +1,9 @@
 package main
 
+/**
+*读取索引文件的工具
+ */
+
 import (
 	"flag"
 	"fmt"
@@ -10,17 +14,21 @@ import (
 )
 
 var (
+	//解析命令行参数
 	indexFileName = flag.String("file", "", ".idx file to analyze")
 )
 
 func main() {
+	//进行解析
 	flag.Parse()
+	//打开文件名
 	indexFile, err := os.OpenFile(*indexFileName, os.O_RDONLY, 0644)
 	if err != nil {
 		log.Fatalf("Create Volume Index [ERROR] %s\n", err)
 	}
 	defer indexFile.Close()
 
+	//一行行的打印索引文件的信息
 	storage.WalkIndexFile(indexFile, func(key uint64, offset, size uint32) error {
 		fmt.Printf("key %d, offset %d, size %d, nextOffset %d\n", key, offset*8, size, offset*8+size)
 		return nil
